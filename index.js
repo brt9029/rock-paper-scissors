cde2caa69d022dea0f8b2832e06d3b259eeb9a9c
brt9029 <brt9029@gmail.com>
2023-06-09 15:15:20 -0700

    added buttons/dom methods to update html instead of using inspect & console.log

--- index.js
+++ index.js
@@ -7,63 +7,68 @@ function computerPlay() {
     return computerSelection[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection, computerSelection) {
+const options = document.querySelector(`.options`);
+// options.addEventListener('click', function(e){
+//     console.log(e.target.textContent.toLowerCase());
+// })
+options.addEventListener('click', playRound)
+
+const result = document.createElement('div');
+const score = document.createElement('div');
+const gameOver = document.createElement('div');
+
+function playRound(e) {
+    let playerSelection = e.target.textContent.toLowerCase();
+    let computerSelection = computerPlay();
+
     let playerWin = "You Win! " + playerSelection + " beats " + computerSelection;
 
     let playerLose = "You Lose! " + computerSelection + " beats " + playerSelection;
-
+    
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
         playerTotal ++;
-        return playerWin;
+        result.textContent = playerWin;
     }
-    if (playerSelection === 'paper' && computerSelection === 'rock') {
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
         playerTotal ++;
-        return playerWin;
+        result.textContent = playerWin;
     }
 
-    if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         playerTotal ++;
-        return playerWin;
+        result.textContent = playerWin;
     }
 
-    if (playerSelection === computerSelection) {
-        return "It's a draw!";
+    else if (playerSelection === computerSelection) {
+        result.textContent = "It's a draw!";
     }
 
     else {
         computerTotal ++;
-        return playerLose;
+        result.textContent = playerLose;
     }
+    options.appendChild(result);
+    result.appendChild(score);
+    score.textContent = "Player score: " + playerTotal + " Computer score: " + computerTotal;
+    if (playerTotal == 5|| computerTotal == 5){
+        checkWins(playerTotal, computerTotal);
+        score.appendChild(gameOver);
+    }
+    
 }
 
 function checkWins(playerScore, computerScore) {
-    let outcome = 'It\'s a tie game! Your score: ' + playerScore + ' Computer score: ' + computerScore;
     if (playerScore > computerScore) {
-        outcome = 'You won the best of 5! Your score: ' + playerScore + ' Computer score: ' + computerScore;
+        outcome = 'You won the best of 5!';
         playerTotal = 0;
         computerTotal = 0;
     } 
 
-    if (playerScore < computerScore) {
-        outcome = 'The computer got the best of you this time...Your score: ' + playerScore + ' Computer score: ' + computerScore;
+    else if (playerScore < computerScore) {
+        outcome = 'The computer got the best of you this time...';
         playerTotal = 0;
         computerTotal = 0;
     }
 
-    else {
-        outcome = 'It\'s a tie game! Your score: ' + playerScore + ' Computer score: ' + computerScore;
-        playerTotal = 0;
-        computerTotal = 0;
-    }
-
-    return outcome;
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = computerPlay();
-        let playerSelection = prompt('Select rock, paper, or scissors!').toLowerCase();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    return console.log(checkWins(playerTotal, playerTotal));
+    return gameOver.textContent = outcome;
 }
